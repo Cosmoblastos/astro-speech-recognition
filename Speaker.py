b9ab741db262c34718e692fe9c0b730a529be7f0
Alejandro Zapett <alejandro.zapett@gmail.com>
2021-09-29 22:55:55 -0500

    bugfix

--- Speaker.py
+++ Speaker.py
@@ -6,15 +6,15 @@ TTS1 = TS.TTS()
 publisher = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 pubsub = publisher.pubsub()
 
+def speaker(Texto):
+    TTS1.Speak(Texto)
+
 pubsub.subscribe("faceIdentification")
 
 for message in pubsub.listen():
     print("Redis: ")
     print(message)
     if (message['channel'] == 'faceIdentification'):
-        print("Eres "+ str(message['data']))
         if(isinstance(message['data'], str)):
-            Speaker("Eres "+ str(message['data']))
-
-def Speaker(Texto):
-    TTS1.Speak(Texto)
+            print("Eres "+ str(message['data']))
+            speaker("Eres "+ str(message['data']))
