8fd3b4416ca3359845ad0ffab26352b742302cd3
Alejandro Gómez García <ale.wild.ones@gmail.com>
2022-05-14 15:40:29 -0500

    add texts to rcp rutine

--- Sistema.py
+++ Sistema.py
@@ -83,7 +83,22 @@ while (Encendido == True):
 
 		if Texto == "emergencia":
 			Tex_Spe.Speak("Hola, soy astro, tu asistente médico personal. ¿Qué edad tiene la persona que necesita mi ayuda?")
-			
+			edad = Spe_Text.Lisen(Duracion)
+			print(edad)
+			#Tex_Spe.Speak(f"El paciente tiene {edad} años")
+			Tex_Spe.Speak("¿Es hombre, o mujer?")
+			sexo = Spe_Text.Lisen(Duracion)
+			print(sexo)
+			#Tex_Spe.Speak(f"El paciente es {sexo}")
+			time.sleep(2)
+			#mostrar video de mapa
+			Tex_Spe.Speak("El paciente está teniendo un paro cardíaco, sus signos vitales se encuentran en mi pantalla y han sido enviados a los servicios de emergencia junto con tu ubicación")
+			time.sleep(2)
+			#mostrar video de emergencia
+			Tex_Spe.Speak("Por favor, colóquese a la altura del hombro del paciente con las rodillas estables en el suelo y con una ligera separación como se ve en mi imagen, comenzará a hacer compresiones")
+			Tex_Spe.Speak("Las manos deben ir enlazadas con la palma de la mano dominante por debajo de la otra mano, para así poder apoyar el talón de la mano con mayor fuerza.")
+			Success = True
+		
 		if "google" in Texto:
 			Consulta = Texto.replace("google","")
 			r.rpush("voiceComands",'google')
